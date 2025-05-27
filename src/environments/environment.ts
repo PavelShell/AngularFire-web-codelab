96590d641bd5bd2d971257d89d024221e6044d09
Pavel Shalianhouski <pshalianhouski@interventioninsights.com>
2025-05-27 22:49:56 +0200

    Added firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
     measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyDgZ13XS-8pP0VJ8BlOjD4yGhkFRWIWrj8",
+    authDomain: "friendlychat-d9c87.firebaseapp.com",
+    projectId: "friendlychat-d9c87",
+    storageBucket: "friendlychat-d9c87.firebasestorage.app",
+    messagingSenderId: "197424916330",
+    appId: "1:197424916330:web:eda77c6f84a0c40e60a713"
   },
 };
